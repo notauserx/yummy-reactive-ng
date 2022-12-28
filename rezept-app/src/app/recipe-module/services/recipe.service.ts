8980aac15a076bc6bb121b2089a2d4b1aea05857
notauserx <notauserx@users.noreply.github.com>
2022-12-28 12:38:20 +0600

    add error handling

--- rezept-app/src/app/recipe-module/services/recipe.service.ts
+++ rezept-app/src/app/recipe-module/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, EMPTY } from 'rxjs';
 import { Recipe } from 'src/app/models/recipe';
 import { environment } from 'src/environment/environment';
 
@@ -8,7 +9,18 @@ const BASE_URL = environment.baseUrl
   providedIn: 'root'
 })
 export class RecipeService {
-  recipeList$ = this.http.get<Recipe[]>(`${BASE_URL}/rezept`);
+  errorMessage = ''
+
+  recipeList$ = this.http
+    .get<Recipe[]>(`${BASE_URL}/rezept`)
+    .pipe(
+      catchError(err => {
+        this.errorMessage = err;
+        console.log("Logging from catchError...");
+        console.log(err);
+        return EMPTY;
+      })
+    );
 
   constructor(private http: HttpClient) { }
 }
