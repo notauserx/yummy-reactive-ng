137dd24dd0aaf065262ca77668188b9474bf5cb1
notauserx <notauserx@users.noreply.github.com>
2023-01-03 11:39:09 +0600

    clean ingredients data

--- rezept-api/Rezept.DataLoader/MappingContexts/RecipeIngredientsMappingContext.cs
+++ rezept-api/Rezept.DataLoader/MappingContexts/RecipeIngredientsMappingContext.cs
@@ -5,12 +5,21 @@ public class RecipeIngredientsMappingContext
     public List<Ingredient> HandleIngredients(string itemNames, string itemQuantities, Guid recipeId)
     {
         var result = new List<Ingredient>();
-        var ingredientQuantities = itemQuantities.BetweenBrackets().Split(",");
-        var ingredientNames = itemNames.BetweenBrackets().Split(",");
+        var ingredientQuantities = itemQuantities
+            .BetweenBrackets()
+            .Split(",")
+            .Select(s => s.Replace("\"", ""))
+            .ToList();
+        
+        var ingredientNames = itemNames
+            .BetweenBrackets()
+            .Split(",")
+            .Select(s => s.Replace("\"", ""))
+            .ToList();
 
-        for (var i = 0; i < ingredientNames.Length; i++)
+        for (var i = 0; i < ingredientNames.Count; i++)
         {
-            if (i >= ingredientQuantities.Length) continue;
+            if (i >= ingredientQuantities.Count) continue;
 
             var ingredientDescription = ingredientQuantities[i] + " " + ingredientNames[i];
 
